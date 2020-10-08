94cb3d62397d90e4181ae4df9d8248cc99a2cdcd
Manjarimam <imam.manjar786@gmail.com>
2020-10-08 16:17:57 +0530

    Updates

---
+++ precache-manifest.8ddec73854ff3411a9a24b20f729d3ac.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e97d168aa081e9d9c2dc55a17dd5f6d",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "4ee3cac161685a8cead2",
+    "url": "/reactwebsite/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "eccf26867f6244ae09d1",
+    "url": "/reactwebsite/static/css/main.f7702335.chunk.css"
+  },
+  {
+    "revision": "4ee3cac161685a8cead2",
+    "url": "/reactwebsite/static/js/2.04764c80.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/reactwebsite/static/js/2.04764c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eccf26867f6244ae09d1",
+    "url": "/reactwebsite/static/js/main.8360d11d.chunk.js"
+  },
+  {
+    "revision": "0bb169a3a757acaebf1e",
+    "url": "/reactwebsite/static/js/runtime-main.d6619741.js"
+  },
+  {
+    "revision": "7a78771b5874cec9f285438461c40bef",
+    "url": "/reactwebsite/static/media/Consulting-Solutions.7a78771b.jpg"
+  },
+  {
+    "revision": "bc8771f8a67fccc99a27928435e14119",
+    "url": "/reactwebsite/static/media/card1.bc8771f8.jpg"
+  },
+  {
+    "revision": "6bb46725399b8dfcb1bcace037f08cd6",
+    "url": "/reactwebsite/static/media/card2.6bb46725.jpg"
+  },
+  {
+    "revision": "0a5ff511775da23e265843d94d16bd0a",
+    "url": "/reactwebsite/static/media/card3.0a5ff511.png"
+  },
+  {
+    "revision": "765a06e689a52a8139c7d80d170f11e1",
+    "url": "/reactwebsite/static/media/card4.765a06e6.jpeg"
+  },
+  {
+    "revision": "e967a7237efdda214432ca365468b8d7",
+    "url": "/reactwebsite/static/media/card5.e967a723.jpg"
+  },
+  {
+    "revision": "14274ed59229806ec6e30f37a9e1fd71",
+    "url": "/reactwebsite/static/media/card6.14274ed5.jpg"
+  },
+  {
+    "revision": "28b4d18901397ac834d7c0d2af543430",
+    "url": "/reactwebsite/static/media/infrastrucure.28b4d189.png"
+  }
+]);
